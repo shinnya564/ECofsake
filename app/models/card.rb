de2837d27ed4f28ac3d974c165e54723c9bfe6b8
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-08-29 13:25:53 +0900

    [add]PAYJPカード番号例外処理

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,13 +4,6 @@ class Card < ApplicationRecord
 
 	require "payjp"
 
-	def card_last4(card)
-		Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-		customer = Payjp::Customer.retrieve(card.customer_id)
-	    default_card_information = customer.cards.retrieve(card.card_id)
-		"**** **** **** " + default_card_information.last4
-  	end
-
 	def cards_last4
 		Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
 		customer = Payjp::Customer.retrieve(self.customer_id)
