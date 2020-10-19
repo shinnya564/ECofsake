f63ac77d083f1b9f6f19eceb92e37b9d01459b06
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-10-19 18:05:58 +0900

    [update]rspec掲示板追加

---
+++ spec/factories/bulletin_boards.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :bulletin_board do
+    title { Faker::Internet.username(specifier: 30) }
+    name { Faker::Internet.username(specifier: 5) }
+    body { Faker::Number.number(digits: 7) }
+  end
+end
---
+++ spec/model/bulletin_boards_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'BulletinBoardsモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    let(:bulletin_board) { create(:bulletin_board) }
+
+    context 'titleカラム' do
+      it '空欄でないこと' do
+        bulletin_board.title = ''
+        expect(bulletin_board.valid?).to eq false;
+      end
+    end
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        bulletin_board.name = ''
+        expect(bulletin_board.valid?).to eq false;
+      end
+    end
+    context 'カラム' do
+      it '空欄でないこと' do
+        bulletin_board.body = ''
+        expect(bulletin_board.valid?).to eq false;
+      end
+    end
+  end
+end
