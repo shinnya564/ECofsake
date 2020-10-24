b90e4ea3221332f21f1bbe87f0d2aa760ed3e74d
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-10-24 16:51:40 +0900

    [update]開発者についてにaddress追 加とTopのスクリプトを移動させ負荷分散

--- app/controllers/admins/end_users_controller.rb
+++ app/controllers/admins/end_users_controller.rb
@@ -15,6 +15,9 @@ class Admins::EndUsersController < ApplicationController
 
   def edit
     @end_user = EndUser.find(params[:id])
+    if @end_user.email == "guest@example.com"
+        redirect_to admins_end_user_path(@end_user), notice:'ゲストユーザーの会員情報は管理者では変更できません' and return
+    end
   end
 
   def update
